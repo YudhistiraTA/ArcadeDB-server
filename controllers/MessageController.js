44b38f055741629127924916eaf61608f6e47ad4
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-07-03 15:43:32 +0700

    Added endpoint to fetch inbox data; Refactored message database structure

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -4,9 +4,33 @@ module.exports = class MessageController {
 	static async messageSend(req, res, next) {
 		try {
 			const { id: senderId } = req.additionalData;
-			let { receiverId, message } = req.body;
-			const messagesRef = admin.database().ref("messages");
-			messagesRef.push({ senderId, receiverId: +receiverId, message });
+			const { receiverId, message } = req.body;
+			if (senderId == receiverId)
+				throw {
+					name: "invalidInput",
+					message: "Can not send message to self"
+				};
+			const senderMessagesRef = admin
+				.database()
+				.ref(`user_messages/${senderId}`);
+			senderMessagesRef.push({
+				senderId,
+				receiverId: +receiverId,
+				message,
+				time: new Date().getTime(),
+				asSender: true
+			});
+
+			const receiverMessagesRef = admin
+				.database()
+				.ref(`user_messages/${receiverId}`);
+			receiverMessagesRef.push({
+				senderId,
+				receiverId: +receiverId,
+				message,
+				time: new Date().getTime(),
+				asSender: false
+			});
 			const messagePayload = {
 				notification: {
 					title: `New message from ${senderId}`,
@@ -24,6 +48,33 @@ module.exports = class MessageController {
 					console.error(error);
 					res.status(500).json({ error: "Failed to send message." });
 				});
+		} catch (error) {
+			console.log(error);
+			next(error);
+		}
+	}
+	static async fetchInbox(req, res, next) {
+		try {
+			const { id } = req.additionalData;
+			const userMessagesRef = admin.database().ref(`user_messages/${id}`);
+			userMessagesRef.once("value", function (snapshot) {
+				const allMessages = snapshot.val();
+				const latestMessages = {};
+				for (const messageId in allMessages) {
+					const message = allMessages[messageId];
+					const partnerId = message.asSender
+						? message.receiverId
+						: message.senderId;
+					if (
+						latestMessages[partnerId] &&
+						latestMessages[partnerId].time > message.time
+					) {
+						continue;
+					}
+					latestMessages[partnerId] = message;
+				}
+				res.status(200).json({ messages: latestMessages });
+			});
 		} catch (error) {
 			console.log(error);
 			next(error);
--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -14,11 +14,12 @@ async function authentication(req,res,next){
         }
         const payload = verifyToken(access_token)
         let user = {}
+        console.log(payload);
         user = await User.findByPk(payload.id)
         if(!user){
             throw {name: 'Unauthenticated', message: "Unauthenticated"}
         }
-
+        console.log(user);
         req.additionalData = {
             id : user.id ,
             username : user.username,
